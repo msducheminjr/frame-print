20eb48336576981cf7c77c6d6d05e424f5489045
Michael Duchemin <msducheminjr@users.noreply.github.com>
2020-12-08 16:31:20 -0500

    Frame-print 0.2.0 to NPM with eslint
    Add eslint to package and fix eslint errors. Update version to 0.2.0.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function print (msg) {
+module.exports = function print(msg) {
   console.log('**********');
   console.log(msg);
   console.log('**********');
--- tests/frame-print.test.js
+++ tests/frame-print.test.js
@@ -1,11 +1,17 @@
+/* eslint-disable */
 const print = require('msducheminjr-frame-print');
+/* eslint-enable */
+
 const originalConsole = console.log;
 let outputData = '';
-storeLog = inputs => (outputData += inputs + '\n');
-console.log('Print is ' + print);
+const storeLog = (inputs) => {
+  (outputData += `${inputs}\n`);
+  return null;
+};
+console.log(`Print is ${print}`);
 test('frame-print prints message with stars above and below', () => {
-  console['log'] = jest.fn(storeLog);
+  console.log = jest.fn(storeLog);
   print('You da boss.');
   expect(outputData).toBe('**********\nYou da boss.\n**********\n');
-  console['log'] = originalConsole;
+  console.log = originalConsole;
 });
